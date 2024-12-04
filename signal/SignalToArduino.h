0617545b469e8932906f5c453048ca7ec5f822c0
pyo <govlhappyday@naver.com>
2024-12-04 21:08:56 +0900

    modify signal code

--- signal/SignalToArduino.h
+++ signal/SignalToArduino.h
@@ -3,3 +3,5 @@
 #include <SerialClass.h>
 
 void SignalToArduino(Serial& arduinoSerial, const char* Command);
+void SignalToArduino(Serial& arduinoSerial, int pwmValue);
+void SignalToArduino(Serial& arduinoSerial, int pwmValue, int delay);
